608567a49c2f48eb10a832a8f0a70617c8beebda
Yoonadi Mon <miyoonadimon@gmail.com>
2026-02-19 11:37:56 +0800

    Yoon: fix file path error (we gotta fix again when he change to php)

--- main/javascript/mainScript.js
+++ main/javascript/mainScript.js
@@ -42,28 +42,28 @@ function updateThemeIcons(isDark) {
     if (toggleBtnMobile) {
         const img1 = toggleBtnMobile.querySelector('img');
         if (img1) {
-            img1.src = isDark ? '../assets/images/dark-mode-icon.svg' : '../assets/images/light-mode-icon.svg';
+            img1.src = isDark ? '../../assets/images/dark-mode-icon.svg' : '../../assets/images/light-mode-icon.svg';
             img1.alt = isDark ? 'Dark Mode Icon' : 'Light Mode Icon';
         }
     }
     if (toggleBtnDesktop) {
         const img2 = toggleBtnDesktop.querySelector('img');
         if (img2) {
-            img2.src = isDark ? '../assets/images/dark-mode-icon.svg' : '../assets/images/light-mode-icon.svg';
+            img2.src = isDark ? '../../assets/images/dark-mode-icon.svg' : '../../assets/images/light-mode-icon.svg';
             img2.alt = isDark ? 'Dark Mode Icon' : 'Light Mode Icon';
         }
     }
     if (settingImg) {
-        settingImg.src = isDark ? '../assets/images/setting-dark.svg' : '../assets/images/setting-light.svg';
+        settingImg.src = isDark ? '../../assets/images/setting-dark.svg' : '../../assets/images/setting-light.svg';
     }
     if (settingImgM) {
-        settingImgM.src = isDark ? '../assets/images/setting-dark.svg' : '../assets/images/setting-light.svg';
+        settingImgM.src = isDark ? '../../assets/images/setting-dark.svg' : '../../assets/images/setting-light.svg';
     }
     if (menuBtn) {
-        menuBtn.src = isDark ? '../assets/images/icon-menu-dark.svg' : '../assets/images/icon-menu.svg';
+        menuBtn.src = isDark ? '../../assets/images/icon-menu-dark.svg' : '../../assets/images/icon-menu.svg';
     }
     if (closeBtn) {
-        closeBtn.src = isDark ? '../assets/images/icon-menu-close-dark.png' : '../assets/images/icon-menu-close.svg';
+        closeBtn.src = isDark ? '../../assets/images/icon-menu-close-dark.png' : '../../assets/images/icon-menu-close.svg';
     }
 }
 
